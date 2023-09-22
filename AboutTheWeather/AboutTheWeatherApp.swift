3e16cba0d9edebf9009baaf7037b41ef3e17a49e
Salvatore Palazzo <salvatop78@gmail.com>
2023-09-22 12:10:19 -0400

    Initial Commit

---
+++ AboutTheWeather/AboutTheWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutTheWeatherApp.swift
+//  AboutTheWeather
+//
+//  Created by little-ac on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct AboutTheWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
