71728b529de7fc703bcae1cf78933d4a84328b41
Salvatore Palazzo <salvatop78@gmail.com>
2023-09-24 16:02:04 -0400

    refactored unit test

--- AboutTheWeatherTests/AboutTheWeatherTests.swift
+++ AboutTheWeatherTests/AboutTheWeatherTests.swift
@@ -8,31 +8,39 @@
 import XCTest
 @testable import AboutTheWeather
 
-class WeatherAppTests: XCTestCase {
+final class AboutTheWeatherTests: XCTestCase {
     
-    final class AboutTheWeatherTests: XCTestCase {
+    var viewModel: WeatherViewModel!
+    var networkManager: NetworkManager!
+    
+    override func setUpWithError() throws {
+        viewModel = WeatherViewModel()
+        networkManager = NetworkManager()
+    }
+    
+    override func tearDownWithError() throws {
+        viewModel = nil
+        networkManager = nil
+    }
+    
+    func testWeatherDataFetch() async throws {
+        let expectation = XCTestExpectation(description: "Fetch weather data")
         
-        var viewModel: WeatherViewModel!
-        
-        override func setUpWithError() throws {
-            viewModel = WeatherViewModel()
-        }
-        
-        override func tearDownWithError() throws {
-            viewModel = nil
-        }
-        
-        func testWeatherDataFetch() async throws {
-            let expectation = XCTestExpectation(description: "Fetch weather data")
-            let endpoint = Endpoint.withLatitudeAndLongitude("45.5019°", "73.5674°")
-            try await viewModel.fetchDataForLocation(from: endpoint)
-            
-            XCTAssertNotNil(viewModel.hourlyData)
-            XCTAssertNotNil(viewModel.dailyData)
-            XCTAssertNotNil(viewModel.locationData)
-        
+        let endpoint = Endpoint.withLatitudeAndLongitude("45.5019°", "73.5674°")
+        let response = try await networkManager.sendRequest(urlString: endpoint.url,
+                                                            mapToDataModel: APIResponse.self)
+        switch response {
+        case .success(let weatherData):
+            XCTAssertNotNil(weatherData)
+            XCTAssertEqual(weatherData.lon, 73.5674)
+            XCTAssertGreaterThan(weatherData.current.temp, -100)
+            XCTAssertNotNil(weatherData.daily.first)
+
             expectation.fulfill()
-            wait(for: [expectation], timeout: 5.0)
+        case .failure(let error):
+            XCTFail("Failed to fetch weather data: \(error.localizedDescription)")
         }
+
+        wait(for: [expectation], timeout: 5.0)
     }
 }
